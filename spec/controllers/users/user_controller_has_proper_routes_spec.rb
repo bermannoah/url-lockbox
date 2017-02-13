60a1c46c97aaf6ec2b0c73666df93653183ac5d9
Noah Berman <noah@noahberman.org>
2017-02-13 12:34:11 -0700

    signup page has proper fields

--- spec/controllers/users/user_controller_has_proper_routes_spec.rb
+++ spec/controllers/users/user_controller_has_proper_routes_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe UsersController, type: :controller do
   context "new" do
-    scenario "it sends users to the right place" do
+    scenario "it sends new users to the right place" do
       get :new
       
       expect(response).to have_http_status(200)
