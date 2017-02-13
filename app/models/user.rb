3a3723535c5f23df41311d6266cbf86fc883b295
Noah Berman <noah@noahberman.org>
2017-02-13 12:36:59 -0700

    add secure password method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+  
+  has_secure_password 
+  
   validates :email, presence: true
   validates :password_digest, presence: true
   validates :email, uniqueness: true
