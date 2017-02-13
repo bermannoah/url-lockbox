b8d286ad4138551cb4509d44f937066b41575666
Noah Berman <noah@noahberman.org>
2017-02-13 12:28:06 -0700

    test for users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: "links#index"
-
+  
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
+  
   resources :links, only: [:index]
 
   namespace :api do
