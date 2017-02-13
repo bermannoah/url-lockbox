9db56a979fa737f2f6935e58604c7551a2a931c4
Noah Berman <noah@noahberman.org>
2017-02-13 14:30:20 -0700

    additional permissions for link and feature test

--- app/models/link.rb
+++ app/models/link.rb
@@ -5,5 +5,7 @@ class Link < ActiveRecord::Base
   validates :url, :url => true
   validates :url, :url => {:no_local => true}
 
+  enum read: [:false, :true]
+
   belongs_to :user
 end
---
+++ spec/features/user_can_see_a_form_for_links_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "seeing a form for links" do
+  context "the user is not logged in" do
+    scenario "and does not see the form" do
+      visit "/links"
+      
+      expect(page).to_not have_content("Add a link here")
+      expect(page).to_not have_content("Submit")
+    end
+  end
+
+  context "the user is logged in" do
+    scenario "and does see the form" do
+      user = Fabricate(:user)
+      stub_logged_in_user(user)
+
+      visit "/links"
+      
+      expect(page).to have_content("Add a link here")
+      expect(page).to have_content("Submit")
+    end
+  end
+end
