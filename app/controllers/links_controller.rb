d391766df79bde412944a3a73169ab9044388ad0
Noah Berman <noah@noahberman.org>
2017-02-13 18:50:29 -0700

    revert

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,12 +3,12 @@ class LinksController < ApplicationController
   
   def create
     link = params["link"]
-    if Link.create(url: link["url"], title: link["title"], user_id: current_user.id)
+    if Link.create!(url: link["url"], title: link["title"], user_id: current_user.id)
       redirect_to '/'
       flash[:notice] = "Link #{link["title"]} Saved!"
     else
-      redirect_to '/'
       flash[:notice] = "You've saved the URL incorrectly. Try again?"
+      redirect_to '/'
     end
   end
   
